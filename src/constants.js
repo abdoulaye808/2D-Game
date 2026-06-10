a77999711f3d59047d2c7f16d092c5539f01fb72
Abdoulaye Diallo <abdoudiall2009@gmail.com>
2026-06-10 16:51:42 -0400

    Finishing touches

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,21 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/abdoulaye808/" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very hard degree to get but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+ `,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@HW5567" target="_blank">HW5567</a>, <a href="https://www.youtube.com/@Yoyo808" target="_blank">Yoyo-808</a>,
+  <a href="https://www.youtube.com/@aarava" target="_blank">aaravae</a> (sometimes) and <a href="https://www.youtube.com/@Eggsterr" target="_blank">eggsterr</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://abdoulaye-profolio.netlify.app/" target="_blank">Check it out?</a>
+  Contact me at abdoudiall2009@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Abdoulaye's portfolio, just close the tab,or <a href="https://abdoulaye-profolio.netlify.app/" target="_blank">go to my resume</a>.`,
+};
